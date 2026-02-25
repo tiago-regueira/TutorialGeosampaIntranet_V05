215b19e40a97e0c2f07779c39c050a89a891f942
tiago-regueira <tiago.regueira.arq@gmail.com>
2026-02-25 11:01:49 -0300

    Primeiro commit

---
+++ Projeto Inicial/README.md
@@ -0,0 +1,7 @@
+# Tutorial GeoSampa
+Este repositório contém o tutorial para uso do GeoSampa em formato de página usando o Github Pages e o Jekyll, tornando o tutorial facilmente acessível por meio de um link e navegável entre seções e subseções.
+
+Link para o tutorial: https://geoinfo-smdu.github.io/tutorial-GeoSampa/tutorial.html
+
+teste-1
+teste
---
+++ tipuesearch/tipuesearch_set.js
@@ -0,0 +1,23 @@
+
+/*
+Tipue Search 3.1
+Copyright (c) 2013 Tipue
+Tipue Search is released under the MIT License
+http://www.tipue.com/search
+*/
+
+
+var tipuesearch_stop_words = ["e", "ser", "por", "fazer", "para", "ele", "ela", "eles", "elas", "como", "se", "é", "isso", "meu", "não", "de", "ou", "o", "a", "os", "as", "com", "o que", "quando"];
+
+var tipuesearch_replace = {"words": [
+     {"word": "tipua", replace_with: "tipue"},
+     {"word": "javscript", replace_with: "javascript"}
+]};
+
+var tipuesearch_stem = {"words": [
+     {"word": "e-mail", stem: "email"},
+     {"word": "javascript", stem: "script"},
+     {"word": "javascript", stem: "js"}
+]};
+
+
